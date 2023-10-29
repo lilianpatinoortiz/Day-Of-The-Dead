5cb8cd66c41e6e373d38753f085d31ed8196cf5f
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-10-29 14:54:07 -0700

    Installing changes

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,24 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// DONE: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false); // show thhe button to install the app
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// DONE: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  const event = window.deferredPrompt; // get event
+  if (!event) {
+    return;
+  }
+  event.prompt();
+  window.deferredPrompt = null; // no event
+  butInstall.classList.toggle("hidden", true); // hide the button for installing the app
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// DONE: Add an handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null; // no event
+});
