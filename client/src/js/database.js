7f5c3d756facb447b2824022e1d7da58b65dede5
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-10-29 15:37:36 -0700

    database changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// DONE: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const deadDb = await openDB("jate", 1);
   const tx = deadDb.transaction("jate", "readwrite");
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
   console.log("Data saved to the database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// DONE: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const deadDb = await openDB("jate", 1);
   const tx = deadDb.transaction("jate", "readonly");
